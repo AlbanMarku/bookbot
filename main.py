11c1751abdc5aef3010cee3443522c1e56d0b335
AlbanMarku <albanmarku@outlook.com>
2025-06-25 18:58:56 +0100

    args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import number_of_words, character_count, sorted_list
+import sys
+from stats import character_count, sorted_list
 def get_book_text(file_path):
     """
     Takes a filepath as input and returns the contents of the file as a string.
@@ -9,12 +10,15 @@ def get_book_text(file_path):
     
     
 def main():
-    # text_content = get_book_text("./books/frankenstein.txt")
-    # word_count = number_of_words(text_content)
-    # print(f"{word_count} words found in the document")
-    book = "./books/frankenstein.txt"
-    book_text = get_book_text(book)
-    chars = character_count(book_text)
-    sorted_list(chars, book, book_text)
+    if len(sys.argv) > 1:
+        book = sys.argv[1]
+        book_text = get_book_text(book)
+        chars = character_count(book_text)
+        sorted_list(chars, book, book_text)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    
 
 main()
